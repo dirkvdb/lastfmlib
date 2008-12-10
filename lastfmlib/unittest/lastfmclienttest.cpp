bc32171f840550ceb133f38ca1fff8a97600bbc2
dirk.vdb <dirk.vdb@c1c17468-f352-0410-a86e-fd3cdf7867e6>
2008-12-10 16:58:32 +0000

    Fixed c interface by initializing time_started value

--- lastfmlib/unittest/lastfmclienttest.cpp
+++ lastfmlib/unittest/lastfmclienttest.cpp
@@ -2,8 +2,10 @@
 
 #include "lastfmlib/lastfmclient.h"
 #include "lastfmlib/lastfmscrobbler.h"
+#include "lastfmlib/lastfmscrobblerc.h"
 #include <ctime>
 #include <unistd.h>
+
 
 using std::string;
 
@@ -11,16 +13,34 @@ SUITE(LastFmClientTest)
 {
     TEST(TestLastFmClient)
     {
-        //~ LastFmScrobbler scrobbler("liblastfmtest", "testlib");
-        //~ SubmissionInfo info(L"Richie Hawtin", L"The Tunnel");
-        //~ info.setTrackLength(35);
-//~
-        //~ scrobbler.startedPlaying(info);
-        //~ sleep(20);
-        //~ scrobbler.finishedPlaying();
+#if 0
+        lastfm_scrobbler* scrobbler = create_scrobbler("liblastfmtest", "testlib", true);
+        set_commit_only_mode(scrobbler, 1);
 
+        submission_info* info = create_submission_info();
+        info->artist = "Richie Hawtin";
+        info->track = "The Tunnel";
+        info->track_length_in_secs = 35;
 
-        //~ LastFmClient lastFm("liblastfmtest", "testlib");
+        started_playing(scrobbler, info);
+        destroy_submission_info(info);
+
+        sleep(20);
+
+        finished_playing(scrobbler);
+        destroy_scrobbler(scrobbler);
+#endif
+#if 0
+        LastFmScrobbler scrobbler("liblastfmtest", "testlib", true);
+        SubmissionInfo info(L"Richie Hawtin", L"The Tunnel");
+        info.setTrackLength(35);
+
+        scrobbler.startedPlaying(info);
+        sleep(20);
+        scrobbler.finishedPlaying();
+#endif
+
+        //~ LastFmClient lastFm("liblastfmtest", "testlib", true);
         //~ SubmissionInfo info(L"Tomaz, Cook-E & Matik, Alter Ego, Ed & Kim", L"Live @ Petrol (03-11-2007)", time(0) - 100);
         //~ info.setTrackLength(12457);
         //~ info.setRating(Love);
