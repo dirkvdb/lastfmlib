f616a31cf811a7f7e7102ef04c12679783cc1c3d
dirk.vdb <dirk.vdb@c1c17468-f352-0410-a86e-fd3cdf7867e6>
2009-03-20 08:13:48 +0000

    Made inclusion guard more specific to avoid problems

--- lastfmlib/utils/log.h
+++ lastfmlib/utils/log.h
@@ -14,8 +14,8 @@
 //    along with this program; if not, write to the Free Software
 //    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
-#ifndef LOG_H
-#define LOG_H
+#ifndef UTILS_LOG_H
+#define UTILS_LOG_H
 
 #ifdef ENABLE_LOGGING
     #include <log4cpp/Category.hh>
